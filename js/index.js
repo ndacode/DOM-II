b0b0f9ddf1f5a217724687134538729a0b423d7d
Nicole Adams <nicoleadams.boston@gmail.com>
2019-10-01 15:40:04 -0400

    Change ideas added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,35 @@
 // Your code goes here
+// * [ ] `mouseover`
+//mouseover the bus image and the whole page design changes.
+
+
+
+// * [ ] `keydown`
+
+
+// * [ ] `wheel`
+//wheel becomes the whole page spins around
+
+// * [ ] `drag / drop`
+//drag the bus over the image and the image takes over the page
+
+
+// * [ ] `load`
+// start up some music on the load
+
+// * [ ] `focus`
+//look up focus
+
+// * [ ] `resize`
+//maybe play the sound of a horn
+
+// * [ ] `scroll`
+//make the header transparent on the scroll
+
+// * [ ] `select`
+// select all makes everything disappear
+
+
+// * [ ] `dblclick`
+
+// dblclick page spin around stops, maybe everything goes back to normal
